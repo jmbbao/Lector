1ce8f959ec1071974057a7fafcd29edb525b9311
jmbbao <manbaleato@gmail.com>
2026-03-12 04:10:30 +0100

    Versión v04

--- program/sw_movil.js
+++ program/sw_movil.js
@@ -1,5 +1,5 @@
 // Nombre del caché específico para la versión móvil
-const CACHE_NAME = "pwa-lector-v03";
+const CACHE_NAME = "pwa-lector-v04";
 
 // Archivos que se guardarán en caché para funcionar offline
 const FILES_TO_CACHE = [
